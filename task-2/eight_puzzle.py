26052caaf9110d3c61addd940b8f5b6036054fac
panpatchara <144796381+panpatchara@users.noreply.github.com>
2023-09-12 18:50:28 +0700

    edit eight_puzzle.py

--- task-2/eight_puzzle.py
+++ task-2/eight_puzzle.py
@@ -50,7 +50,7 @@ def h1(s):
 
 def h3(s):
     # implement this function
-   goal = (1, 2, 3, 4, 5, 6, 7, 8, 0)
+    goal = (1, 2, 3, 4, 5, 6, 7, 8, 0)
     board, _, _ = s
     res = 0
     for idx in range(1, 9):
